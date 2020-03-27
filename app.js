8e99b52ed3db719316d6e15d0c2058f63c123035
Samuel Walker <sewalker191@gmail.com>
2020-03-27 16:08:03 -0700

    Add debug scripts to package.json

--- app.js
+++ app.js
@@ -4,12 +4,21 @@ const port = process.env.PORT || 5000;
 
 const mongoose = require('mongoose');
 const mongoURI = require('./config/keys.js').mongoURI
-
+console.log('STARTING SERVER');
 
 mongoose.connect(mongoURI, { useNewUrlParser: true, useUnifiedTopology: true })
     .then(() => console.log('Connected to MongoDB successfully!'))
     .catch(error => console.log(error));
 
-app.get('/', (req, res) => res.send('Hello Dawg!'));
+app.get('/test', (req, res) => {
+    console.log('tester');
+    res.json('what is happening');
+});
+
+app.get("/", (req, res) => {
+    console.log('banana');
+    res.json('HOME');
+})
+
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
