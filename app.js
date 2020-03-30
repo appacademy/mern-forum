7784ec83458430be940b68114beaa88f5b2988e3
Samuel Walker <sewalker191@gmail.com>
2020-03-30 14:32:22 -0700

    Finish setup

--- app.js
+++ app.js
@@ -4,19 +4,12 @@ const port = process.env.PORT || 5000;
 
 const mongoose = require('mongoose');
 const mongoURI = require('./config/keys.js').mongoURI
-console.log('STARTING SERVER');
 
 mongoose.connect(mongoURI, { useNewUrlParser: true, useUnifiedTopology: true })
     .then(() => console.log('Connected to MongoDB successfully!'))
     .catch(error => console.log(error));
 
-app.get('/test', (req, res) => {
-    console.log('tester');
-    res.json('what is happening');
-});
-
 app.get("/", (req, res) => {
-    console.log('banana');
     res.json('HOME');
 })
 
